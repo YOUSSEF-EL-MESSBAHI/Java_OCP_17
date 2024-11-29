fe94b6fef4187dddd1e8cbaab5f4185f839cc66e
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2024-11-29 23:52:10 +0100

    28/11

---
+++ src/StringHandling.java
@@ -0,0 +1,15 @@
+public class StringHandling {
+
+    public static void main(String[] args) {
+        String str = "Hello World";
+        String str2 = "Hello World";
+        System.out.println(str == str2);
+        System.out.println(str.equals(str2));
+
+        str.concat("test");
+        StringBuilder stringBuilder = new StringBuilder("Hello World");
+        stringBuilder.append("test");
+        System.out.println(stringBuilder.toString() +"|"+str);
+
+    }
+}
