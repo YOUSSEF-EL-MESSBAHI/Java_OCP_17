dea09c7be702b4324b2425f056439906ed2f8211
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2024-12-07 23:53:09 +0100

    07/12

--- src/Streams/ReduceDemo.java
+++ src/Streams/ReduceDemo.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 public class ReduceDemo {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(3, 7, 8, 1, 5, 9);
+        List<String> words = Arrays.asList("corejava", "spring", "hibernate");
+
         Integer reduceSum = numbers.stream().reduce(2, (a, b) -> a + b);
         System.out.println(reduceSum);
         Optional<Integer> reduceSumWithMethodReference = numbers.stream().reduce(Integer::sum);
@@ -14,6 +16,15 @@ public class ReduceDemo {
 
         Integer mulResult = numbers.stream().reduce(1, (a, b) -> a * b);
         System.out.println(mulResult);
+
+        Integer maxvalueWithMethodReference = numbers.stream().reduce(Integer::max).get();
+        System.out.println(maxvalueWithMethodReference);
+
+
+        String longestString = words.stream()
+                .reduce((word1, word2) -> word1.length() > word2.length() ? word1 : word2)
+                .get();
+        System.out.println(longestString);
 
         Integer maxvalue = numbers.stream().reduce(0, (a, b) -> a > b ? a : b);
         System.out.println(maxvalue);
