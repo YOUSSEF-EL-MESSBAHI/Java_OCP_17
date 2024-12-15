d32349805b765df3f720f7bddb4248a67484c5bd
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2024-12-15 19:56:16 +0100

    15/12

--- src/DataType.java
+++ src/DataType.java
@@ -7,6 +7,10 @@ public class DataType {
         byte c = (byte) b;
         long d = a;
         float f = 3.14f;
+        int s= 0b10;
+        System.out.println(s);
+        short x=0xff;
+        System.out.println(x);
 
     }
 }
