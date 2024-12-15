605a4595360f193e96a21b7c527b8899c3f6a228
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2024-12-15 18:17:08 +0100

    15/12

--- src/Streams/PrimitiveStreams.java
+++ src/Streams/PrimitiveStreams.java
@@ -6,11 +6,11 @@ import java.util.stream.Stream;
 
 public class PrimitiveStreams {
     public static void main(String[] args) {
-//        Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5);
-//        long sum = integerStream.reduce(0, (a, b) -> a + b);
-//        System.out.println(sum);
-//        long count = integerStream.count();
-//        System.out.println(sum/count);
+        Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5);
+        long sum = integerStream.reduce(0, (a, b) -> a + b);
+        System.out.println(sum);
+        long count = integerStream.count();
+        System.out.println(sum/count);
 
         IntStream intStream = IntStream.of(1, 2, 3, 4, 5);
 //        long sum2 = intStream.reduce(0, (a, b) -> a + b);
