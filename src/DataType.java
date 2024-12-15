0923d294d3974b142787a8ef0dff76683bc95f03
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2024-12-15 19:04:29 +0100

    15/12

---
+++ src/DataType.java
@@ -0,0 +1,12 @@
+public class DataType {
+    public static void main(String[] args) {
+        byte a = 127;
+        a='b';
+        long b = 123L;
+        b='c';
+        byte c = (byte) b;
+        long d = a;
+        float f = 3.14f;
+
+    }
+}
--- src/Streams/PrimitiveStreams.java
+++ src/Streams/PrimitiveStreams.java
@@ -1,23 +1,29 @@
 package Streams;
 
 import java.util.OptionalDouble;
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class PrimitiveStreams {
     public static void main(String[] args) {
-        Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5);
-        long sum = integerStream.reduce(0, (a, b) -> a + b);
-        System.out.println(sum);
-        long count = integerStream.count();
-        System.out.println(sum/count);
+//        Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5);
+//        integerStream.min(Integer::compare);
+//        long sum = integerStream.reduce(0, (a, b) -> a + b);
+//        System.out.println(sum);
+//        long count = integerStream.count();
+//        System.out.println(sum/count);
 
         IntStream intStream = IntStream.of(1, 2, 3, 4, 5);
 //        long sum2 = intStream.reduce(0, (a, b) -> a + b);
 //        long count2 = intStream.count();
         intStream.average().ifPresent(System.out::println);
         Stream<Integer> integerStream = IntStream.of(1, 2).boxed();
-        integerStream.forEach(System.out::println);
+        Stream<Long> boxed = LongStream.of(1L, 2L).boxed();
+        boxed.forEach(System.out::println);
+        OptionalInt min = intStream.min();
+        OptionalInt max = intStream.max();
 
     }
 }
