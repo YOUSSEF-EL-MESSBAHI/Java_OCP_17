213c20ac1881e14eb75919dd9290eaab1c736e8b
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2024-12-17 22:12:39 +0100

    17/12

--- src/DataType.java
+++ src/DataType.java
@@ -1,16 +1,24 @@
 public class DataType {
     public static void main(String[] args) {
-        byte a = 127;
-        a='b';
-        long b = 123L;
-        b='c';
-        byte c = (byte) b;
-        long d = a;
-        float f = 3.14f;
-        int s= 0b10;
-//        System.out.println(s);
-        short x=0xff;
-//        System.out.println(x);
-
+//        byte a = 127;
+//        a='b';
+//        long b = 123L;
+//        b='c';
+//        byte c = (byte) b;
+//        long d = a;
+//        float f = 3.14f;
+//        int s= 0b10;
+////        System.out.println(s);
+//        short x=0xff;
+////        System.out.println(x);
+//        boolean v=true;
+//        System.out.println(!v);
+        short b=3;
+        short c=4 ;
+//        b=b+c; //b now is int
+        ++b;
+        float d= 1.6f;
+        int f = (int) d;
+        System.out.println(f);
     }
 }
