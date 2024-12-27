abfa1d883d0ddd8cc2576eadc8d96c211fecf7b4
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2024-12-28 00:34:23 +0100

    28/12

--- src/switchEvolution/switchDemo.java
+++ src/switchEvolution/switchDemo.java
@@ -48,4 +48,99 @@ public class switchDemo {
         }
     }
 
+    public void switchCasejava8Enums(){
+        DAYS days = DAYS.SUNDAY;
+        switch (days) {
+            case MONDAY:
+                System.out.println("Weekdays");
+                break;
+            case TUESDAY:
+                System.out.println("Weekdays");
+                break;
+            case WEDNESDAY:
+                System.out.println("Weekdays");
+                break;
+            case THURSDAY:
+                System.out.println("Weekdays");
+                break;
+            case FRIDAY:
+                System.out.println("Weekdays");
+                break;
+            case SATURDAY:
+                System.out.println("Weekends");
+                break;
+            case SUNDAY:
+                System.out.println("Weekends");
+                break;
+            default:
+                System.out.println("Unknown");
+        }
+    }
+
+    public String switchCasejava13(String day){
+        return  switch (day) {
+            case "Monday":
+                yield  "Weekday";
+            case "Tuesday":
+                yield "Weekday";
+            case "Wednesday":
+                yield "Weekday";
+            case "Thursday":
+                yield "Weekday";
+            case "Friday":
+                yield "Weekday";
+            case "Saturday":
+                yield "Weekend";
+            case "Sunday":
+                yield "Weekend";
+            default:
+                yield "Unknown";
+        };
+    }
+
+    public String switchCasejava13Arrow(String day){
+        return  switch (day) {
+            case "Monday"-> "Week day";
+            case "Tuesday"-> "Week day";
+            case "Wednesday"->"Week day";
+            case "Thursday"->"Week day";
+            case "Friday"->"Week day";
+            case "Saturday"-> "Weekend";
+            case "Sunday"-> "Weekend";
+            default->"Unknown";
+        };
+    }
+
+    public String switchCasejava13Multi(String day){
+        return  switch (day) {
+            case "Monday","Tuesday","Wednesday","Thursday","Friday"
+                    -> "Week day";
+            case "Saturday", "Sunday" -> "Weekend";
+            default->"Unknown";
+        };
+    }
+
+    public String switchCasejava14(Object obj){
+        return switch (obj) {
+            case Integer i -> "It is an integer";
+            case String s -> "It is a string";
+//            case Employee s -> "It is a Employee";
+            default -> "It is none of the known data types";
+        };
+    }
+
+//    public String switchCasejava15(Object obj){
+//        return switch (obj) {
+//            case Integer i -> "It is an integer";
+//            case String s -> "It is a string";
+//            case Employee employee && employee.getDept().equals("IT") -> "IT Employee";
+//                default -> "It is none of the known data types";
+//        };
+//    }
+
+//    case null -> "It is a null object";
+
+}
+enum DAYS {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
 }
