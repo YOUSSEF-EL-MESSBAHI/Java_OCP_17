5cd09ed14a1c93f8117785a243c5affde4d341f8
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2024-12-28 00:26:05 +0100

    28/12

---
+++ src/switchEvolution/switchDemo.java
@@ -0,0 +1,51 @@
+package switchEvolution;
+
+public class switchDemo {
+
+    public static void main(String[] args) {
+
+    }
+    public void switchCasejava7(){
+        int value = 5;
+        switch(value)
+        {
+            case 1:
+                System.out.println("One");
+                break;
+            case 5:
+                System.out.println("five");
+                break;
+            default:
+                System.out.println("Unknown");
+        }
+    }
+    public void switchCasejava8(){
+        String day = "Tuesday";
+        switch (day) {
+            case "Monday":
+                System.out.println("Week day");
+                break;
+            case "Tuesday":
+                System.out.println("Week day");
+                break;
+            case "Wednesday":
+                System.out.println("Week day");
+                break;
+            case "Thursday":
+                System.out.println("Week day");
+                break;
+            case "Friday":
+                System.out.println("Week day");
+                break;
+            case "Saturday":
+                System.out.println("Weekend");
+                break;
+            case "Sunday":
+                System.out.println("Weekend");
+                break;
+            default:
+                System.out.println("Unknown");
+        }
+    }
+
+}
