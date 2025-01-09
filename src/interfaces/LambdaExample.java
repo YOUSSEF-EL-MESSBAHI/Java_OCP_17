d2bcce3dc7f394592a6def3a28563e4a1c870647
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2025-01-09 22:34:32 +0100

    janvier

--- src/interfaces/LambdaExample.java
+++ src/interfaces/LambdaExample.java
@@ -7,6 +7,13 @@ public class LambdaExample {
         MyInterface myInterface = (var x, var y) -> x + y;
         int sum = myInterface.add(5, 3);
         System.out.println("Sum: " + sum); // Output: Sum: 8
+
+//        x is effectively final
+//        (x) -> {
+//            x = x + 1; // Compilation error
+//            return x;
+//        }
+
     }
 }
 
