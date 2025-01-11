3442520125bc2fc2d14afd17613767ae9d3b2bc3
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2025-01-11 23:45:11 +0100

    janvier

--- src/examples/MyRessourceTest.java
+++ src/examples/MyRessourceTest.java
@@ -1,11 +1,27 @@
 package examples;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class MyRessourceTest {
     public static void main(String[] args) {
-        try (AutoCloseable autoCloseable= new MyRessource()) {
+//        try (AutoCloseable autoCloseable= new MyRessource()) {
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
 
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        var x = List.of("hdfhdud");// not accept nulls
+        var y = Set.of(2,3); // not accept redandant values
+        var z = Arrays.asList("hdfhdud");
+        var d = Map.of("1", "GFG",
+                "2", "Geek",
+                "3", "GeeksForGeeks");
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(d);
     }
 }
