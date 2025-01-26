897b813fbec3468d274ac197dd27f8351e678fc5
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2025-01-26 17:40:38 +0100

    janvier

---
+++ src/java17Exam/Example17.java
@@ -0,0 +1,22 @@
+package java17Exam;
+
+import java.time.LocalDate;
+
+public class Example17 {
+    public static void main(String[] args) {
+
+    }
+    public String test(Object obj) {
+//        return switch (obj) {
+//            case LocalDate date when date.isBefore(LocalDate.now()) ->
+//                    "Past";
+//            case LocalDate date when date.isAfter(LocalDate.now()) ->
+//                    "Future";
+//            case LocalDate date when date.equals(LocalDate.now()) ->
+//                    "Present";
+//            default -> "Wrong Value";
+//        };
+        return "for java 21";
+    }
+
+}
