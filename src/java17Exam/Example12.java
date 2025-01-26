6791b47663fd4c3b35f46bcc7194831f24bfbf7e
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2025-01-26 13:24:37 +0100

    janvier

---
+++ src/java17Exam/Example12.java
@@ -0,0 +1,22 @@
+package java17Exam;
+
+class OClass {
+    private int var1 = 100;
+    public int var2 = 200;
+    class IClass {
+        int var3 =300;
+        public int var4=400;
+        int m2() {
+            return var2;
+        }
+    }
+    public  int m1(){
+        return var1;
+    }
+}
+public class Example12 {
+    public static void main(String[] args) {
+        OClass.IClass iClass= new OClass().new IClass();
+        // iClass can access var3 var4 m2()
+    }
+}
