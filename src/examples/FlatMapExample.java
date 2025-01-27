df3fbce7dccb56488738cd1f31ca0a09fabf31c5
YOUSSEF-EL-MESSBAHI <youssefelmesbahi30@gmail.com>
2025-01-27 22:12:19 +0100

    janvier

--- src/examples/FlatMapExample.java
+++ src/examples/FlatMapExample.java
@@ -9,7 +9,7 @@ public class FlatMapExample {
         List<List<List<Integer>>> deepNested = Arrays.asList(
                 Arrays.asList(Arrays.asList(1, 2), Arrays.asList(3, 4)),
                 Arrays.asList(Arrays.asList(5, 6), Arrays.asList(7, 8))
-        );
+        );// array of array of array
 
         List<Integer> collect = deepNested.stream()
                 .flatMap(List::stream)
